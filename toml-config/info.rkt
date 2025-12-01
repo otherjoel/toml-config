36c31c49058f0a3b83bca17f0bcd350d5bfc8fd1
Joel Dueck <joel@jdueck.net>
2025-11-30 22:13:09 -0600

    docs: multi page

--- toml-config/info.rkt
+++ toml-config/info.rkt
@@ -5,7 +5,7 @@
 (define deps '("base"
                "toml-config-lib"))
 (define implies '("toml-config-lib"))
-(define scribblings '(("scribblings/toml-config.scrbl" () (parsing-library))))
+(define scribblings '(("scribblings/toml-config.scrbl" (multi-page) (parsing-library))))
 (define pkg-desc "A #lang for validating TOML files")
 (define version "2.0")
 (define pkg-authors '("Joel Dueck"))
